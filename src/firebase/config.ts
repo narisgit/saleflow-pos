c0812152477818a84a639d2bcc43843dcbe091d1
นริศรา เอี่ยมคณิตชาติ <narissarae@gmail.com>
2026-02-26 08:24:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4164623446-99f88",
+  "appId": "1:1035381062496:web:1621318a353920206c3c05",
+  "apiKey": "AIzaSyDuUqmJFzJYE75AID9U42rH-0KgXMJB8WA",
+  "authDomain": "studio-4164623446-99f88.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1035381062496"
+};
